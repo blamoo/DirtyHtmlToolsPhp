5424166a6c1e77e7c0dbeab5cb6b4773df23729a
Luis Henrique Soler <luis.soler@solutudo.com.br>
2017-05-22 16:00:54 -0300

    Correção de bug

--- src/DirtyHtmlTools/Sanitize.php
+++ src/DirtyHtmlTools/Sanitize.php
@@ -4,13 +4,14 @@ namespace DirtyHtmlTools;
 
 class Sanitize
 {
-	public static function filterTags($data, array $rules) {
+	public static function filterTags($data, array $rules)
+	{
 		if (is_string($data)) {
 			$data = Parser::Parse($data);
 		}
-		
+
 		$rulesFixed = array();
-		
+
 		foreach ($rules as $key => $val) {
 			if (is_int($key)) {
 				$rulesFixed[$val] = array();
@@ -18,27 +19,32 @@ class Sanitize
 				$rulesFixed[$key] = $val;
 			}
 		}
-			
+
 		$root = new Tag();
 		$root->children = $data;
 		$root->isRoot = true;
-		
+
 		self::filterTag($root, $rulesFixed);
-			
+
 		return Element::Html($data);
 	}
-	
-	private static function filterTag(Tag $tag, array $rules) {
+
+	private static function filterTag(Tag $tag, array $rules)
+	{
 		if (!$tag->isRoot) {
 			if (!isset($rules[$tag->name])) {
 				$tag->filtered = true;
 			}
 		}
-		
-		$tag->attributes = array_filter($tag->attributes, function($key, $val) use ($rules) {
-			return isset($rules[$key]);
+
+		$tag->attributes = array_filter($tag->attributes, function ($val, $key) use ($rules, $tag) {
+			if (!isset($rules[$tag->name]) || !is_array($rules[$tag->name])) {
+				return false;
+			}
+
+			return in_array($key, $rules[$tag->name]);
 		}, ARRAY_FILTER_USE_BOTH);
-		
+
 		foreach ($tag->children as $val) {
 			if ($val instanceof Tag) {
 				self::filterTag($val, $rules);
